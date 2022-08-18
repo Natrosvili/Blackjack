8c6b5158238e2edd2945612b00ac77c89ee0385b
Natrosvili <100809251+Natrosvili@users.noreply.github.com>
2022-08-18 19:13:20 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random 
-userIn = True
-computerIn = True
+import random
+
+spieler = True
+computer = True
 
 
 cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10,
@@ -13,15 +14,29 @@ cards = [2, 3, 4, 5, 6, 7, 8, 9, 10, 10,
         'J' ,'K', 'Q', 'A'
 ]
 
-user = []
-computer = []
 
+SPIELER = []
+COMPUTER = []
+
+
+
+# Diese Funktion wählt eine zufällige Karte aus, 
+# fügt dem Argument der Funktionsaufrufe turn 
+# hinzu und entfernt sie aus der Liste der Karten, 
+# damit sie nicht noch einmal verwendet wird
 def dealCard(turn):
     card = random.choice(cards)
     turn.append(card)
     cards.remove(card)
 
 
+
+
+
+# Diese Funktion hat ein Argument namens turn, 
+# erstellt eine Variable namens total und ein Array namens face. 
+# und macht die Logik hinter den J-, K- und Q-Karten, die im echten 
+# Blackjack-Kartenspiel existieren
 def total(turn):
     total = 0
     face = ['J', 'K', 'Q']
@@ -39,59 +54,72 @@ def total(turn):
 
 
 
-def revealComputer ():
-    if len(user) == 2:
-        return computer[0]
-    elif len(computer) > 2:
-        return computer[0], computer[1]
+spielerTotal = total(SPIELER)
+computerTotal = total(COMPUTER)
 
 
 
+def revealComputer():
+    if spielerTotal == 2:
+        return COMPUTER[0]
+    elif computerTotal > 2:
+        return COMPUTER[0], COMPUTER[1]
+
+
+
+# Diese For-Schleife veranlasst sowohl den Spieler
+# als auch den Computer, zufällige Karten mit der Funktion 
+# dealCard zu erhalten, und ruft das Argument als Spieler und Computer auf
 for _ in range(2):
-    dealCard(computer)
-    dealCard(user)
+    dealCard(COMPUTER)
+    dealCard(SPIELER)
 
 
-while userIn and computerIn:
-    print("Hallo und Herzlich Wilkommen zum Blackjack!")
+
+
+
+
+
+while spieler and computer:
+    print("* * * Hallo und Herzlich Wilkommen zum Blackjack! * * *")
     u = input("möchten sie spielen?\n1 -> Ja\n2 -> Nein\nAntwort: ")
     if u == "1" or u == "j" or u == "J" or u == "ja" or u == "Ja" or u == "JA":   
         print(f"der Computer hat: {revealComputer()} und X")
-        print(f"Sie haben {user} für insgesamt {total(user)}")
-        if userIn:
+        print(f"Sie haben {SPIELER} für insgesamt {spielerTotal}")
+        if spieler:
             stayOrHit = input("1 -> hit\n2 -> Stay\n")
-        if total(computer) > 16:
-            computerIn = False
+        if computerTotal > 16:
+            computer = False
         else:
-            dealCard(computer)
+            dealCard(COMPUTER)
         if stayOrHit == "2":
-            userIn = False
+            spieler = False
         else:
-            dealCard(user)
-        if total(user) >= 21:    
+            dealCard(SPIELER)
+        if spielerTotal >= 21:    
             break
-        elif total(computer) >= 21:
+        elif computerTotal >= 21:
             break
 
-        if total(user) == 21:
-            print(f"\nSie haben {total(user)} und den Computer hat {total(computer)}")
+        if spielerTotal == 21:
+            print(f"\nSie haben {spielerTotal} und den Computer hat {computerTotal}")
             print("Blackjack! Sie haben gewonnen!")
-        elif total(computer) == 21:
-            print(f"\nSie haben {total(user)} und den Computer hat {total(computer)}")
+        elif computerTotal== 21:
+            print(f"\nSie haben {spielerTotal} und den Computer hat {computerTotal}")
             print("Blackjack! der Computer hat gewonnen!")
-        elif total(user) > 21:
-            print(f"\nSie haben {total(user)} und den Computer hat {total(computer)}")
+        elif spielerTotal > 21:
+            print(f"\nSie haben {spielerTotal} und den Computer hat {computerTotal}")
             print("Sie sind besiegt! computer hat gewonnen")
-        elif total(computer) > 21:
-            print(f"\nSie haben {total(user)} und den Computer hat {total(computer)}")
+        elif computerTotal > 21:
+            print(f"\nSie haben {spielerTotal} und den Computer hat {computerTotal}")
             print("Computer zerquetscht! Sie haben gewonnen!")
-        elif 21 - total(computer) < 21 - total(user):
-            print(f"\nSie haben {total(user)} und den Computer hat {total(computer)}")
+        elif 21 - computerTotal < 21 - spielerTotal:
+            print(f"\nSie haben {spielerTotal} und den Computer hat {computerTotal}")
             print("Computer hat gewonnen!")
-        elif 21 - total(computer) > 21 - total(user):
-            print(f"\nSie haben {total(user)} und den Computer hat {total(computer)}")
+        elif 21 - computerTotal > 21 - spielerTotal:
+            print(f"\nSie haben {spielerTotal} und den Computer hat {computerTotal}")
             print("Sie haben gewonnen!")
     elif u == "2" or u == "n" or u == "N" or u == "nein" or u == "Nein" or u == "NEIN":
         print("Schade! Schönnen Tag noch.")
-        userIn = False
-        computerIn = False
+        spieler = False
+        computer = False
